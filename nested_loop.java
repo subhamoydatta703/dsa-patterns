f2af1a7d60d2d2a15f9e7934244f1fdf38914c69
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-21 22:52:54 +0530

    added another function

--- nested_loop.java
+++ nested_loop.java
@@ -3,14 +3,22 @@ public class nested_loop {
     static void patt(int x){
         for(int i=0; i<x; i++){
             for(int j=0; j<x; j++){
-                System.err.print("*");
+                System.err.print("* ");
             }
             System.err.println();
         }
     }
 
+    static void patt_2(int x){
+        for(int i=0; i<x; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        patt(4
-        );
+        patt_2(4);
     }
 }
